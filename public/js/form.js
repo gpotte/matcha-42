1223a838929fe4b6169970c00d9f67974fd3a4b3
Gilles POTTE <gpotte@e1r3p15.42.fr>
2017-07-13 10:36:07 +0200

    aesthetic work

---
+++ public/js/form.js
@@ -0,0 +1,19 @@
+$('#username').focus(()=> {
+  $('#usernameCond').removeClass("inactive");
+  $('#usernameCond').addClass('active');
+});
+
+$('#username').blur(()=>{
+  $('#usernameCond').removeClass("active");
+  $('#usernameCond').addClass('inactive');
+});
+
+$('#password').focus(()=> {
+  $('#passwordCond').removeClass("inactive");
+  $('#passwordCond').addClass('active');
+});
+
+$('#password').blur(()=>{
+  $('#passwordCond').removeClass("active");
+  $('#passwordCond').addClass('inactive');
+});
